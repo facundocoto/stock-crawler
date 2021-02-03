48c6f37fb86cf34a3a8d33fd541606428795a945
Facundo <facundocoto@hotmail.com>
2021-02-02 22:59:32 -0300

    Log

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const HTMLParser = require("node-html-parser");
 const { URLS } = require("./constants");
 
 async function main() {
+  console.log("Buscando stock");
   const { data: htmlAsString } = await axios.get(URLS.FRAVEGA_XBOX_SERIES_X);
 
-  const fravegaTo = parsedHtml.text(
+  const parsedHtml = HTMLParser.parse(htmlAsString);
+  const fravegaStock = parsedHtml.querySelector(
     "#__next > div:nth-child(4) > div > div > div:nth-child(2) > section"
   );
+  if (!fravegaStock) console.log("No hay stock pa");
 }
 
 main();
