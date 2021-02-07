75267b9dc532dcb9cd23bfacb7689905639eed37
Facundo <facundocoto@hotmail.com>
2021-02-07 12:24:07 -0300

    Add more options and use cheerio instead of html-parser

--- constants.js
+++ constants.js
@@ -1,6 +1,12 @@
 const URLS = {
   FRAVEGA_XBOX_SERIES_X:
     "https://www.fravega.com/p/consola-microsoft-xbox-series-x-1-tb-342420",
+  ATAJO_XBOX_SERIES_S:
+    "https://www.atajo.com.ar/consolas-xbox-series-s--det--RRS-00002",
+  ATAJO_XBOX_SERIES_X_KIT:
+    "https://www.atajo.com.ar/consolas-xbox-series-x-kit--det--RRT-00002_KIT01",
+  GARBARINO_SERIES_S:
+    "https://www.garbarino.com/producto/consola-microsoft-xbox-series-s-512gb-blanco/906efaab77",
 };
 
 module.exports = { URLS };
--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 const axios = require("axios");
-const HTMLParser = require("node-html-parser");
+const cheerio = require("cheerio");
 
 const { URLS } = require("./constants");
 
-async function main() {
-  console.log("Buscando stock");
+async function fravega() {
   const { data: htmlAsString } = await axios.get(URLS.FRAVEGA_XBOX_SERIES_X);
 
-  const parsedHtml = HTMLParser.parse(htmlAsString);
-  const fravegaStock = parsedHtml.querySelector(
-    "#__next > div:nth-child(4) > div > div > div:nth-child(2) > section"
+  const parsedHtml = cheerio.load(htmlAsString);
+  const fravegaStockLabel = parsedHtml(
+    "#__next > div.withMainLayout__LayoutWrapper-sc-1lj5zpr-1.hhFziS > div > div > div.ProductDetails__NotAvailableWrapper-sc-2486oi-0.cxXFXL > section > p"
   );
-  if (!fravegaStock) console.log("No hay stock pa");
+  return fravegaStockLabel.text() !== "Producto temporalmente sin stock";
+}
+
+async function atajo_series_s() {
+  const { data: htmlAsString } = await axios.get(URLS.ATAJO_XBOX_SERIES_S);
+
+  const parsedHtml = cheerio.load(htmlAsString);
+  const atajoStockLabel = parsedHtml(
+    "#detalle > div.col-lg-6.col-md-6.col-sm-6.detalle_producto > h3.leyendaSinStock"
+  );
+
+  return atajoStockLabel.text() !== "Sin Stock";
+}
+
+async function atajo_series_x() {
+  const { data: htmlAsString } = await axios.get(URLS.ATAJO_XBOX_SERIES_X_KIT);
+
+  const parsedHtml = cheerio.load(htmlAsString);
+  const atajoStockLabel = parsedHtml(
+    "#detalle > div.col-lg-6.col-md-6.col-sm-6.detalle_producto > h3.leyendaSinStock"
+  );
+
+  return atajoStockLabel.text() !== "Sin Stock";
+}
+
+async function garbarino() {
+  const { data: htmlAsString } = await axios.get(URLS.GARBARINO_SERIES_S);
+
+  const parsedHtml = cheerio.load(htmlAsString);
+  const garbarinoStockLabel = parsedHtml(
+    "body > div.gb-wrapper > div.gb-products-carousel > div > div.gb-vintage-carousel-head > div.gb-vintage-carousel-head-image > span"
+  );
+
+  return garbarinoStockLabel.text() !== "AGOTADO";
+}
+
+async function main() {
+  console.log("Buscando stock...");
+  const atajoStockSeriesS = await atajo_series_s();
+  const atajoStockSeriesX = await atajo_series_x();
+  const fravegaStock = await fravega();
+  const garbarinoStock = await garbarino();
+
+  console.log({
+    Fravega: { SeriesX: fravegaStock },
+    Atajo: { SeriesX: atajoStockSeriesX, SeriesS: atajoStockSeriesS },
+    Garbarino: { SeriesS: garbarinoStock },
+  });
 }
 
 main();
